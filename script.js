da788689ab3e7af8862d39c6ebe2ce27f4847660
annaodinokovait <50737755+Student123568@users.noreply.github.com>
2024-10-06 20:28:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = "Люблю тебя"
+});
